f0412c46e62c944d21c7aca20982242645931b17
Roberto Alvarez <robertoatc02@gmail.com>
2014-05-01 20:30:01 -0700

    add simple animmation

--- mouse/app.js
+++ mouse/app.js
@@ -3,11 +3,28 @@
 var settings = {
   width: 500,
   height: 500,
+  minR: 2,
+  maxR: 4,
+  pi: Math.PI,
+  outerRad: 100
 };
 
 var random = function(min,max){
   return ( Math.random() * max ) + min;
 };
+
+
+var posX = function(x){
+
+  var angle = random(0, 2 * settings.pi);
+  return x + settings.outerRad * Math.cos(angle);
+};
+
+var posY = function(y){
+
+  var angle = random(0, 2 * settings.pi);
+  return y - settings.outerRad * Math.sin(angle);
+}
 
 var svg = d3.selectAll("#chart").append("svg")
   .attr("width", settings.width)
@@ -17,14 +34,23 @@ var svg = d3.selectAll("#chart").append("svg")
 
 d3.selectAll('svg').on('mousemove', function(){
   
+  var coords = d3.mouse(this);
+
+  
+
+
   svg.selectAll('circle')
-     .data(d3.range(3))
+     .data(d3.range(20))
      .enter()
      .append('circle')
-     .attr('r', 3)
-     .attr('cx', 5)
-     .attr('cy', 5)
-
+     .attr('r', function() { return random(settings.minR, settings.maxR); })
+     .attr('cx', function() { return posX(coords[0]); })
+     .attr('cy', function() { return posY(coords[1]); })
+     .transition()
+     .duration(1000)
+     .attr('cx', coords[0])
+     .attr('cy', coords[1])
+     .remove();
 
 });
 
